128d87370fd4acbb4f1f62084e91bfe6a361bcf6
giuseppe88-sketch <giuseppeadamo25@yahoo.com>
2021-05-12 13:57:32 +0200

    import react

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import './index.scss';
 
 //Main component
 
-class MyMoviesApplication extends React.component{
+class MyMoviesApplication extends React.Component{
     render(){
         return (
         <div className="my-flix">
